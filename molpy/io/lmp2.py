1c8db6a61ddf0349a74f3b714ea34c2b3e44502e
Roy Kid <lijichen365@126.com>
2022-03-13 14:52:24 +0800

    rewrite lammps data input

--- molpy/io/lmp2.py
+++ molpy/io/lmp2.py
@@ -9,10 +9,12 @@ __all__ = [
     'DumpReader'
 ]
 
+from typing import List
 import numpy as np
 from molpy.io.fileHandler import FileHandler
 
-DECLEAR_TYPES = {
+DUMP_TYPES = {
+    
     'id': int,
     'mol': int,
     'type': int,
@@ -20,7 +22,9 @@ DECLEAR_TYPES = {
     'x': float,
     'y': float,
     'z': float,
+
 }
+
 
 class DumpReader:
     
@@ -30,14 +34,14 @@ class DumpReader:
         self.filehandler = FileHandler(fpath)
         self.chunks = self.filehandler.readchunks('ITEM: TIMESTEP')
         
-    def getFrame(self, index):
+    def getframe(self, index):
         
         chunk = self.chunks.getchunk(index)
         
         return DumpReader.parse(chunk)
         
     @staticmethod
-    def parse(chunk:list[str]):
+    def parse(chunk:List[str]):
         
         data = {}
         
@@ -52,9 +56,161 @@ class DumpReader:
         
         m = map(lambda x: tuple(x.split()), chunk[9:])
         lm = list(m)
-        atomArr = np.array(lm, dtype={'names': header, 'formats': [DECLEAR_TYPES[k] for k in header]})
+        atomArr = np.array(lm, dtype={'names': header, 'formats': [DUMP_TYPES[k] for k in header]})
         
         data['atomInfo'] = atomArr
         
         return data
+       
+       
+SECTIONS = [
+    'Masses', 'Atoms', 'Bonds', 'Angles', 'Dihedrals', 'Impropers'
+]
+
+DATA_TYPES = {
+    'atoms': int,
+    'bonds': int,
+    'angles': int,
+    'dihedrals': int,
+    'impropers': int,
+    'atom types': int,
+    'bond types': int,
+    'angle types': int,
+    'dihedral types': int,
+    'improper types': int,
+}
+           
+class DataReader:
+    
+    def __init__(self, fpath:str, atom_style:str='full'):
+        
+        self.filepath = fpath
+        self.filehander = FileHandler(fpath)
+        self.atom_style = atom_style
+        
+    def getdata(self):
+        
+        lines = self.filehander.readlines()
+        data = DataReader.parse(lines, self.atom_style)
+        return data
+        
+    @staticmethod
+    def parse_line(line:str):
+        
+        return line.partition('#')[0].split()
+        
+    @staticmethod
+    def parse(lines:List[str], atom_style:str='full'):
+        
+        data = {}
+        
+        data['comment'] = lines[0]
+        
+        for i, line in enumerate(lines[2:14]):
             
+            line = DataReader.parse_line(line)
+            
+            for field, type in DATA_TYPES.items():
+                
+                if line[-1] == field:
+                    data[field] = type(line[0])
+                    break
+                
+            if line[-1] == 'xhi':
+                break
+                
+        for line in lines[i+2:i+5]:
+            
+            line = DataReader.parse_line(line)
+            
+            if line[-1] == 'xhi':
+                xlo = float(line[0])
+                xhi = float(line[1])
+                
+            elif line[-1] == 'yhi':
+                ylo = float(line[0])
+                yhi = float(line[1])
+                
+            elif line[-1] == 'zhi':
+                zlo = float(line[0])
+                zhi = float(line[1])
+        
+        data['box'] = [xlo, xhi, ylo, yhi, zlo, zhi]
+        
+        section_start_lineno = {}
+        
+        for lino, line in enumerate(lines):
+            
+            line = DataReader.parse_line(line)
+            if line and line[0] in SECTIONS:
+                section_start_lineno[line[0]] = lino
+           
+                    
+        #--- parse atoms ---
+        atom_section_starts = section_start_lineno['Atoms'] + 2
+        atom_section_ends = atom_section_starts + data['atoms']
+        
+        atomInfo = DataReader.parse_atoms(lines[atom_section_starts:atom_section_ends], atom_style=atom_style)
+        data['Atoms'] = atomInfo
+                    
+        #--- parse bonds ---
+        if 'Bonds' in section_start_lineno:
+            bond_section_starts = section_start_lineno['Bonds'] + 2
+            bond_section_ends = bond_section_starts + data['bonds'] + 1
+            
+            bondInfo = DataReader.parse_bonds(lines[bond_section_starts:bond_section_ends])
+            data['Bonds'] = bondInfo
+        
+        # #--- parse angles ---
+        if 'Angles' in section_start_lineno:
+            angles_section_starts = section_start_lineno['Angles'] + 2
+            angles_section_ends = angles_section_starts + data['angles'] + 1
+            angleInfo = DataReader.parse_angles(lines[angles_section_starts:angles_section_ends])
+            data['Angles'] = angleInfo
+        
+        # #--- parse dihedrals ---
+        if 'Dihedrals' in section_start_lineno:
+            dihedrals_section_starts = section_start_lineno['Dihedrals'] + 2
+            dihedrals_section_ends = dihedrals_section_starts + data['dihedrals'] + 1
+            dihedralInfo = DataReader.parse_dihedrals(lines[dihedrals_section_starts:dihedrals_section_ends])
+            data['Dihedrals'] = dihedralInfo
+        
+        
+        return data
+        
+    @staticmethod
+    def parse_atoms(lines:List[str], atom_style='full'):
+        
+        atom_style_types = {
+            'full': np.dtype([
+                ('id', int),
+                ('mol', int),
+                ('type', int),
+                ('q', float),
+                ('x', float),
+                ('y', float),
+                ('z', float)
+            ])
+        }
+        
+        l = list(map(lambda x: tuple(x.split()), lines))
+        
+        atomInfo = np.array(l, dtype=atom_style_types[atom_style])
+        
+        return atomInfo
+    
+    @staticmethod
+    def parse_bonds(lines:List[str]):
+        
+        return list(map(lambda x: list(map(int, x.split())), lines))
+    
+    @staticmethod
+    def parse_angles(lines:List[str]):
+        
+        return list(map(lambda x: list(map(int, x.split())), lines))
+
+    @staticmethod
+    def parse_dihedrals(lines:List[str]):
+        
+        return list(map(lambda x: list(map(int, x.split())), lines))
+    
--- tests/test_io/test_lmp2.py
+++ tests/test_io/test_lmp2.py
@@ -4,7 +4,7 @@
 # version: 0.0.1
 
 import pytest
-from molpy.io.lmp2 import DumpReader
+from molpy.io.lmp2 import DumpReader, DataReader
 import numpy.testing as npt
 import numpy as np
 
@@ -18,10 +18,37 @@ class TestDump:
         
     def test_get_frame(self, dump):
         
-        data = dump.getFrame(2)
+        data = dump.getframe(2)
         assert data['timestep'] == 20
         assert data['natoms'] == 60
         atomInfo = data['atomInfo']
         npt.assert_allclose(np.asfarray(list(atomInfo[-1])), np.array('45 18 6 -11.8 2.37502 43.1304 46.726'.split(), dtype=float))
         
+class TestData:
+    
+    @pytest.fixture(scope='class', name='data')
+    def test_init(self):
+        
+        data = DataReader('tests/data/linear.data')
+        yield data
+        
+    def test_parse_line(self):
+        
+        line = '\t60 Atom\n# comment'
+        line = DataReader.parse_line(line)
+        assert line == ['60', 'Atom']
+        
+        line = ''
+        line = DataReader.parse_line(line)
+        assert line != ['']
+        
+    def test_parse(self, data):
+        
+        data = data.getdata()
+        atoms = data['Atoms']
+        
+        npt.assert_allclose(np.asfarray(list(atoms[-1])), np.array('60	33	6	-11.8000	40.2637	47.8022	 6.3059'.split(), dtype=float))
+        
+        bonds = data['Bonds']
+        assert bonds[0] == [1, 1, 1, 2]
         
