199482a97ad43d2b55a20b4b38ff0229028ab150
WangXinyan940 <wangxy940930@gmail.com>
2022-06-23 12:11:27 +0800

    Update demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -8,20 +8,20 @@ xmlParser.parse("test/gaff-2.11.xml", 'test/lig-prm.xml')
 xmlParser.write('test/test1.xml')
 
 # feature 2: get specific nodes
-atomtypes = xmlParser.select("ForceField/AtomTypes/Type")
-residues = xmlParser.select("ForceField/Residues/Residue")
+atomtypes = fftree.select("ForceField/AtomTypes/Type")
+residues = fftree.select("ForceField/Residues/Residue")
 
 # feature 3: get specific attributes as list
-r0_charges = xmlParser.select("ForceField/Residues/Residue[0]/Atom<charge>")
-harmonic_bond_lengths = xmlParser.select("ForceField/HarmonicBondForce/Bond<length>")
+r0_charges = fftree.select("ForceField/Residues/Residue[0]/Atom<charge>")
+harmonic_bond_lengths = fftree.select("ForceField/HarmonicBondForce/Bond<length>")
 
 # feature 4: overwrite parameters
-xmlParser.overwrite("ForceField/NonbondedForce<lj14scale>", [0.4])
-xmlParser.overwrite("ForceField/Residues/Residue[0]/Atom<charge>", r0_charges * 0.8)
-xmlParser.overwrite("ForceField/HarmonicBondForce/Bond<length>", harmonic_bond_lengths * 0.9)
+fftree.overwrite("ForceField/NonbondedForce<lj14scale>", [0.4])
+fftree.overwrite("ForceField/Residues/Residue[0]/Atom<charge>", r0_charges * 0.8)
+fftree.overwrite("ForceField/HarmonicBondForce/Bond<length>", harmonic_bond_lengths * 0.9)
 
 # feature 5: raise error if the length of values and patched nodes does not match
 try:
-    xmlParser.overwrite("ForceField/Residues/Residue[0]/Atom<charge>", r0_charges[:-2])
+    fftree.overwrite("ForceField/Residues/Residue[0]/Atom<charge>", r0_charges[:-2])
 except SelectError as e:
     print("Raise Error", e)
